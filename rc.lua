ca2397dd6c599f1c38421ab0162ee022007789c1
Don Coughlin <DCElemeno@gmail.com>
2018-09-05 10:29:17 -0400

    smaller tweaks

--- rc.lua
+++ rc.lua
@@ -241,7 +241,7 @@ awful.screen.connect_for_each_screen(function(s)
             mybatterybar,
             require("battery-widget") {}, sprtr,
             mytextclock, sprtr,
-            mymainmenu,
+            mylauncher,
             --s.mylayoutbox,--
         },
     }
